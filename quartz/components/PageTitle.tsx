03a728f934771038959905103222df25f241750a
PigeonRationel <cardinalsouffle@protonmail.com>
2025-06-11 09:36:15 -0400

    Quartz sync: Jun 11, 2025, 9:36 AM

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -15,7 +15,7 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
 
 PageTitle.css = `
 .page-title {
-  font-size: 5em;
+  font-size: 1.75rem;
   margin: 0;
   font-family: var(--titleFont);
 }
